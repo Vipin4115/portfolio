097016a9bfe2b7b1008ae2e98383fab9d6fdfecb
Vipin4115 <vipinsaibanavath@gmail.com>
2025-07-15 17:35:13 +0530

    last2  commit

--- src/Components/WorkCardData.js
+++ src/Components/WorkCardData.js
@@ -1,21 +1,22 @@
 import pro1 from "../assets/airbnb.png";
-import pro2 from "../assets/project2.png";
+import pro2 from "../assets/urlshortner.png";
 import pro3 from "../assets/project3.png";
 
 const ProjectCardData = [
     {
         imgsrc: pro1,
         title: "Airbnb Website",
-        text: "A modern Airbnb-style platform to discover unique stays around the world.",
+        text: "A modern Airbnb-style platform to discover unique stays around the world.Built with full-stack technologies, it features secure user authentication, dynamic property listings, and interactive maps for seamless booking experiences.",
         view: "https://airbnb-luen.onrender.com/listings",
-        source: "https://github.com/Vipin4115/airbnb",
+        source: "https://github.com/Vipin4115/airbnb"
 
     },
     {
         imgsrc: pro2,
-        title: "Ecommerce Website Design",
-        text: "Thirving Facebook community and top-ranked podcast.",
-        view: "https://www.youtube.com/watch?v=P8YuWEKTeuE",
+        title: "URL Shortener",
+        text: "A fast and minimal URL shortener that lets users generate and manage short links with ease.",
+        view: "https://url-eta-self.vercel.app/auth",
+        source: "https://github.com/Vipin4115/URL-Shortner"
     },
     {
         imgsrc: pro3,
